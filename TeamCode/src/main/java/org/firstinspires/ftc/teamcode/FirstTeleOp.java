f177103d5ed8cac541661f66f11999f0d40746a6
Tomaz2323 <lima.tomaz@escola.pr.gov.br>
2025-09-25 20:39:32 -0300

    Bumper e velocidade variável no TeleOp.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleOp.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class FirstTeleOp extends OpMode {
+    @Override
+    public void init() {
+        //Somente acontece uma vez
+    }
+
+    @Override
+    public void start() {
+        //Acontece depois do programa começar
+    }
+
+    @Override
+    public void loop() {
+        //Acontece repetidamente durante o programa
+    }
+
+    @Override
+    public void stop() {
+        //Acontece quando para
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearTeleOp2.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name = "First_Teleop2")
+public class LinearTeleOp2 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Initialization
+        DcMotorEx motorOne;
+        motorOne = hardwareMap.get(DcMotorEx.class, "motor_one");
+
+        motorOne.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        int degrees = 105;
+        double grau_motor = motorOne.getCurrentPosition();
+        boolean isPressed = false;
+        double vel = 0.2;
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            if(gamepad1.left_stick_y > -0.2 && gamepad1.left_stick_y < 0.2){
+                motorOne.setPower(0);
+                motorOne.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            }
+
+            if(gamepad1.left_stick_y > 0.2){
+                motorOne.setPower(vel);
+            }
+
+            if(gamepad1.left_stick_y < -0.2){
+                motorOne.setPower(-vel);
+            }
+
+            if(gamepad1.right_bumper && !isPressed){
+                vel += 0.2;
+                isPressed = true;
+            }else if(!gamepad1.right_bumper){
+                isPressed = false;
+            }
+
+        }
+    }
+}
